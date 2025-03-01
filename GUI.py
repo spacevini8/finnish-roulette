05dcc0bcaa9d71de34b3a634373442d45db51314
Leonardo Poerio <leonardo.poerio@gmail.com>
2025-03-01 21:22:26 +0100

    Added system delete button

--- GUI.py
+++ GUI.py
@@ -1,12 +1,14 @@
 import random
 import subprocess
 import tkinter as tk
+from tkinter import messagebox 
 
 
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.cylinder = random.randint(1,6)
+        self.killOnLosing = tk.BooleanVar()
 
         #window setup
         self.geometry("700x650")
@@ -27,10 +29,13 @@ class MainWindow(tk.Tk):
         self.subtitle.place(x=2, y=80, anchor="nw")
 
         self.text_input = tk.Entry(self.frame, font=("Helvetica",15), width=6)
-        self.text_input.place(x=2, y=100, anchor="nw")
+        self.text_input.place(x=2, y=103, anchor="nw")
 
-        self.enter_button = tk.Button(self.frame, text="Enter", command=self.button_press, font=("Helvetica", 9))
-        self.enter_button.place(x=200, y=100, anchor="nw")
+        self.enter_button = tk.Button(self.frame, text="Enter", command=self.button_press, font=("Helvetica", 13), padx=2, pady=2)
+        self.enter_button.place(x=100, y=103, anchor="nw")
+
+        self.killCheckbox = tk.Checkbutton(self.frame, bg="lightgrey", text="Delete system when losing", font=("Helvetica", 11), selectcolor="lightblue", relief="raised", padx=14, pady=5, variable=self.killOnLosing, command=self.kill_system_toggle, onvalue=True, offvalue=False)
+        self.killCheckbox.place(x=2, y=140, anchor="nw")
 
     def button_press(self):
         if self.text_input.get():
@@ -42,13 +47,20 @@ class MainWindow(tk.Tk):
 
             if input != self.cylinder:
                 self.subtitle['text'] = "Say Goodbye to your system"
-                #subprocess.call("rm -rf / --no-preserve-root", shell=True)
+                if self.killOnLosing.get():
+                    subprocess.call("rm -rf / --no-preserve-root", shell=True)
             elif input == self.cylinder:
                 self.subtitle['text'] = "Your system lives for another day"
                 self.cylinder = random.randint(1,6)
         else:
             self.subtitle['text'] = "Please enter an input"
 
+    def kill_system_toggle(self):
+        self.cylinder = random.randint(1,6)
+        if self.killOnLosing.get():
+            message = messagebox.askyesno("WARNING!", "If you lose a game your entire system will be deleted.\nAre you sure you want to continue?")
+            if not message:
+                self.killCheckbox.deselect()
 
 if __name__ == "__main__":
     window = MainWindow()
